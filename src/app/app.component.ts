ff7f6906accc568d282b9d0e48147e1182d4324d
mehdi-keshavarz <mk2954751@gmail.com>
2023-10-26 11:59:32 +0330

    working with input decorator and interface and geting data at parent in child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { COURSES } from 'src/db-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-corse-2';
+
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxjsCourse = COURSES[2];
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.scss']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course!: Course;
+
+  constructor() {
+
+  };
+
+  ngOnInit() {
+
+  }
+}
