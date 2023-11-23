94b07d862a881fd92f37c69a585d51edcf4b6cac
mehdi80 <mk2954751@gmail.com>
2023-11-23 14:17:32 +0330

    useing the elvise operator

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 export class CourseCardComponent implements OnInit {
 
   @Input()
-  course!: Course;
+  course!: Course | undefined;
 
   @Input()
   cardIndex!: number;
