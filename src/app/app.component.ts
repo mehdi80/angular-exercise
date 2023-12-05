5aa3ebbef05a847144df1fc19e88b1d370ee7965
mehdi80 <mk2954751@gmail.com>
2023-12-05 17:26:20 +0330

    useing the view children decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Query, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { COURSES } from 'src/db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -14,15 +14,27 @@ export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
-  @ViewChild('cardRef1', { read: ElementRef }) Card1!: ElementRef;
-
-  @ViewChild('courseImage') corseImage!: ElementRef;
+  @ViewChildren(CourseCardComponent, { read: ElementRef, })
+  cards!: QueryList<ElementRef>;
 
   constructor() {
   }
 
   ngAfterViewInit() {
-    console.log("courseImage", this.corseImage);
+    console.log(this.cards)
+  }
+
+  onCourseEdited() {
+    this.courses.push(
+      {
+        id: 2,
+        description: "RxJs In Practice Course",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+        longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+        category: 'BEGINNER',
+        lessonsCount: 10
+      }
+    )
   }
 
   onCourseCelected(course: Course) {
