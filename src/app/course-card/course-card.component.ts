d4ee4bc86a8e34254391e832993e31a474b3777b
mehdi80 <mk2954751@gmail.com>
2024-01-16 11:18:12 +0330

    use shadowDom

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -6,7 +6,7 @@ import { CourseImageComponent } from '../course-image/course-image.component';
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class CourseCardComponent implements OnInit, AfterViewInit {
 
